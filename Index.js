c6213c6b7c5ee515f48e277fd9136215dc1060fd
Stavios <ngoystavios@gmail.com>
2023-06-29 13:04:07 +0200

    fixed  displaying result logic

--- Index.js
+++ Index.js
@@ -1,23 +1,35 @@
-import { interestCal } from "../function.js"
+import { monthlyPaymentResult, interestCal } from "../function.js"
 
 const yearValue = document.querySelector("#progressValue");
 const period_el = document.querySelector("#loanPeriod-el");
 const calcBtn = document.querySelector("#calcBtn");
 const interestInput = document.querySelector("#interest-el");
+const amount_el = document.querySelector("#loanAmount-el");
 
-let amount_el =  document.querySelector("#loanAmount-el");
+let progressValue
 
-interestInput.setAttribute("disabled",true);
-interestInput.value = interestCal() + "%";
-yearValue.textContent = ": 0 years"
+interestInput.setAttribute("disabled", true);
+yearValue.textContent = 0
 
 
 period_el.addEventListener('input', function (e) {
-    yearValue.textContent = `: ${e.target.value} years`;
+  progressValue = e.target.value;
+  yearValue.textContent = `${progressValue} years`
+
 })
 
-calcBtn.addEventListener('click', function () {
-    console.log(amount_el.value);
+
+calcBtn.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (amount_el.value === "") {
+    amount_el.style.border = "1px solid red"
+    alert("please fill in the field")
+  } else {
+    amount_el.style.border = "0px solid inherit"
+    console.log(monthlyPaymentResult(amount_el.value, progressValue).toFixed(0)); // months
+
+  }
+
 })
 
 
--- function.js
+++ function.js
@@ -1,10 +1,18 @@
-export function interestCal(){
-    const interestRate = 11.75 / 12 
-    return interestRate;
+export function interestCal() {
+    const interestRateCalc = 11.75 / 100;
+    const interestFinalCalc = interestRateCalc / 12
+    return Number(interestFinalCalc.toFixed(5))
 }
 
-export function getNumOfPayment(years){
+export function getMonthlyNumOfPayment(years) {
     return years * 12
 }
 
+
+export function monthlyPaymentResult(loanAmount, year) {
+    const annualInterestRate = Number(interestCal() * Math.pow(1 + interestCal(), getMonthlyNumOfPayment(year)).toFixed(5))
+    const annualInterestRateSub = Number((Math.pow(1 + interestCal(), getMonthlyNumOfPayment(year)) - 1).toFixed(5))
+    const finalResult = loanAmount * (annualInterestRate / annualInterestRateSub)
+    return Math.round(finalResult)
+}
 
