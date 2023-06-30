f58844baa7c4df52124a1b3f2824b9c3cd086073
Stavios <ngoystavios@gmail.com>
2023-06-30 14:58:51 +0200

    deposit feature and fix graph bugs
    Added deposit input and fix styling of graph and logic

--- Index.js
+++ Index.js
@@ -1,36 +1,60 @@
-import { monthlyPaymentResult, interestCal } from "../function.js"
+import { monthlyPaymentResult, drawCircle } from "../function.js"
 
 const yearValue = document.querySelector("#progressValue");
 const period_el = document.querySelector("#loanPeriod-el");
 const calcBtn = document.querySelector("#calcBtn");
 const interestInput = document.querySelector("#interest-el");
 const amount_el = document.querySelector("#loanAmount-el");
+const ctx = document.getElementById('myChart').getContext("2d");
 
+const showResult_el = document.querySelector(".showResultContainer");
+const installment_el = document.querySelector("#installment");
+
+const additional_el = document.querySelector("#deposit_el");
+const additional_container = document.querySelector("#deposit_container");
+const depositInput = document.querySelector("#deposit_input");
+
+
+//init
 let progressValue
-
-interestInput.setAttribute("disabled", true);
+let interestRate = 0
 yearValue.textContent = 0
+let loanDeposit = 0
 
 
 period_el.addEventListener('input', function (e) {
   progressValue = e.target.value;
-  yearValue.textContent = `${progressValue} years`
+  yearValue.textContent = `${progressValue} yrs`
 
 })
 
 
-calcBtn.addEventListener('click', function (event) {
-  event.preventDefault()
-  if (amount_el.value === "") {
-    amount_el.style.border = "1px solid red"
+calcBtn.addEventListener('click', function () {
+  interestRate = interestInput.value
+  loanDeposit = depositInput.value
+  if (amount_el.value === "" || interestInput.value === "") {
     alert("please fill in the field")
   } else {
+    const payment = monthlyPaymentResult(amount_el.value, interestRate, progressValue, loanDeposit).toFixed(0)
+    installment_el.textContent = `R${payment}`
+    showResult_el.style.visibility = "visible"
     amount_el.style.border = "0px solid inherit"
-    console.log(monthlyPaymentResult(amount_el.value, progressValue).toFixed(0)); // months
-
+    drawCircle(ctx, amount_el.value, loanDeposit)
   }
 
+
 })
+
+additional_el.addEventListener("click", function () {
+  additional_container.classList.toggle("deposit_container")
+})
+
+
+
+
+
+
+
 
 
 
--- function.js
+++ function.js
@@ -1,5 +1,5 @@
-export function interestCal() {
-    const interestRateCalc = 11.75 / 100;
+export function interestCal(interest) {
+    const interestRateCalc = Number(interest / 100);
     const interestFinalCalc = interestRateCalc / 12
     return Number(interestFinalCalc.toFixed(5))
 }
@@ -9,10 +9,45 @@ export function getMonthlyNumOfPayment(years) {
 }
 
 
-export function monthlyPaymentResult(loanAmount, year) {
-    const annualInterestRate = Number(interestCal() * Math.pow(1 + interestCal(), getMonthlyNumOfPayment(year)).toFixed(5))
-    const annualInterestRateSub = Number((Math.pow(1 + interestCal(), getMonthlyNumOfPayment(year)) - 1).toFixed(5))
-    const finalResult = loanAmount * (annualInterestRate / annualInterestRateSub)
-    return Math.round(finalResult)
+
+export function monthlyPaymentResult(loanAmount, interest, year, deposit) {
+    const annualInterestRate = Number(interestCal(interest) * Math.pow(1 + interestCal(interest), getMonthlyNumOfPayment(year)).toFixed(5))
+    const annualInterestRateSub = Number((Math.pow(1 + interestCal(interest), getMonthlyNumOfPayment(year)) - 1).toFixed(5))
+    let finalResult
+
+    // adding deposit filter
+    if (deposit <= 0) {
+        deposit = 0
+        finalResult = loanAmount * (annualInterestRate / annualInterestRateSub)
+        return Math.round(finalResult)
+    } else {
+        finalResult = (loanAmount - deposit) * (annualInterestRate / annualInterestRateSub);
+        return Math.round(finalResult)
+    }
+
+
 }
 
+export function drawCircle(canvaId, HomeAmount, deposit) {
+
+    //charts
+    const canvas = new Chart(canvaId, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [HomeAmount, deposit],
+                borderWidth: 1
+            }],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+            ],
+            labels: [
+                'Home Amount',
+                'Deposit'
+            ]
+        }
+    });
+
+
+}
